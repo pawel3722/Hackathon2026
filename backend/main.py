3a2896d55e2a178f01e7cf6942b122d7fefe4752
unknown <p.schmidt@onet.eu>
2026-04-18 18:03:10 +0200

    Polaczenie fronta do lobby

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,16 @@ import models
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+     CORSMiddleware,
+     allow_origins=["*"],  # Allows all origins
+     allow_credentials=True,
+     allow_methods=["*"],  # Allows all methods
+     allow_headers=["*"],  # Allows all headers
+ )
+
 @app.get("/")
 def serve_index():
     return FileResponse("index.html")
