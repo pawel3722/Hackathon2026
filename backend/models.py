0391f822c571e8c4b9484844c0c7b8a67386e32f
krzsztfwtk <96871836+krzsztfwtk@users.noreply.github.com>
2026-04-18 12:19:04 +0200

    crypto and stock models update

--- backend/models.py
+++ backend/models.py
@@ -24,24 +24,35 @@ class Lobby:
         self.lock = asyncio.Lock()
 
 class Stock:
-    def __init__(self, id, name, type, price, number_of_shares):
+    def __init__(self, id, ticker, name, type, price, number_of_shares, growth, risk, market_sensitivity, book_value_floor):
         self.id = id
+        self.ticker = ticker
         self.name = name
         self.type = type #taki enum, np. fuel, food, media
         self.price = price
         self.number_of_shares = number_of_shares
+        self.growth = growth
+        self.risk = risk
+        self.market_sensitivity = market_sensitivity
+        self.book_value_floor = book_value_floor
 
 class Crypto:
-    def __init__(self, id, name, price):
+    def __init__(self, id, ticker, name, price, growth, risk, market_sensitivity):
         self.id = id
+        self.ticker = ticker
         self.name = name
         self.price = price
+        self.growth = growth
+        self.risk = risk
+        self.market_sensitivity = market_sensitivity
 
 class Property:
-    def __init__(self, id, name, price):
+    def __init__(self, id, name, price, rent, energy_use):
         self.id = id
         self.name = name
         self.price = price
+        self.rent = rent
+        self.energy_use = energy_use
 
 class Credit:
     def __init__(self, id, price, number_of_instalments):
