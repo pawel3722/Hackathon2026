80705f0f34cc89392b6d779fd85642b0b78e58a0
krzsztfwtk <96871836+krzsztfwtk@users.noreply.github.com>
2026-04-18 18:57:01 +0200

    fix game_state errors

--- backend/game_state.py
+++ backend/game_state.py
@@ -89,25 +89,27 @@ class GameState:
                         if player_crypto.quantity == 0:
                             current_player.cryptos.remove(player_crypto)
 
-        elif field["type"] == "bank" and action.action_type == "bank":
-            if action.actions_type == "credit" and action.assets_id in [1, 2, 3]:
-                if action.assets_id == 1:
-                    credit = Credit(amount=action.amount, instalment_rate=action.amount * 1.15 / 12 , number_of_instalments=12)
-                elif action.assets_id == 2:
-                    credit = Credit(amount=action.amount, instalment_rate=action.amount * 1.125 / 24, number_of_instalments=24)
-                elif action.assets_id == 3:
-                    credit = Credit(amount=action.amount, instalment_rate=action.amount * 1.1 / 36, number_of_instalments=36)
-                current_player.money += action.amount
-                current_player.credits.append(credit)
-            elif(action.action_type == "deposit" and action.assets_id in [4, 5, 6]):
-                if action.assets_id == 4:
-                    deposit = Deposit(amount=action.amount, lending_rate=action.amount * 1.06, number_of_instalments=12)
-                elif action.assets_id == 5:
-                    deposit = Deposit(amount=action.amount, lending_rate=action.amount * 1.065, number_of_instalments=24)
-                elif action.assets_id == 6:
-                    deposit = Deposit(amount=action.amount, lending_rate=action.amount * 1.07, number_of_instalments=36)
-                current_player.money -= action.amount
-                current_player.deposits.append(deposit)
+        elif field["type"] == "bank" and any(a.action_type == "bank" for a in move.actions):
+            bank_actions = [a for a in move.actions if a.action_type == "bank"]
+            for action in bank_actions:
+                if action.assets_type == "credit" and action.assets_id in [1, 2, 3]:
+                    if action.assets_id == 1:
+                        credit = Credit(amount=action.amount, instalment_rate=action.amount * 1.15 / 12 , number_of_instalments=12)
+                    elif action.assets_id == 2:
+                        credit = Credit(amount=action.amount, instalment_rate=action.amount * 1.125 / 24, number_of_instalments=24)
+                    elif action.assets_id == 3:
+                        credit = Credit(amount=action.amount, instalment_rate=action.amount * 1.1 / 36, number_of_instalments=36)
+                    current_player.money += action.amount
+                    current_player.credits.append(credit)
+                elif action.assets_type == "deposit" and action.assets_id in [4, 5, 6]:
+                    if action.assets_id == 4:
+                        deposit = Deposit(amount=action.amount, lending_rate=action.amount * 1.06, number_of_instalments=12)
+                    elif action.assets_id == 5:
+                        deposit = Deposit(amount=action.amount, lending_rate=action.amount * 1.065, number_of_instalments=24)
+                    elif action.assets_id == 6:
+                        deposit = Deposit(amount=action.amount, lending_rate=action.amount * 1.07, number_of_instalments=36)
+                    current_player.money -= action.amount
+                    current_player.deposits.append(deposit)
 
         for deposit in current_player.deposits:
             deposit.number_of_instalments -= 1
